7735c4ba3cc2401f85111409f7e4157bed876881
Ronald Espitia <ronald79@gmail.com>
2024-08-28 18:27:27 -0500

    Clase Agosto 28

---
+++ agosto27.js
@@ -0,0 +1,20 @@
+// prompt
+
+// let nombre = parseInt(prompt('Dame tu nombre'));
+
+// console.log(typeof nombre);
+
+// // alert
+// alert(nombre);
+
+// console.log('LINEA DESPUES DEL PROMPT');
+
+// while 
+
+let contador = 1;
+
+while (contador < 5) {
+    console.log(contador);
+    contador++;
+}
+
