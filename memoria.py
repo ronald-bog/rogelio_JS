544570486e0a48a94d6c083c3ac3a448cb0df167
Ronald Espitia <ronald79@gmail.com>
2025-02-04 21:59:16 -0500

    clase

---
+++ memoria.py
@@ -0,0 +1,16 @@
+
+arreglo1 = [1, 2]
+arreglo2 = [1, 2]
+
+print('****************************')
+print(arreglo1)
+print(arreglo2)
+print('****************************')
+print(id(arreglo1))
+print(id(arreglo2))
+
+arreglo1.append(20)
+
+print('****************************')
+print(arreglo1)
+print(arreglo2)
--- oct14.js
+++ oct14.js
@@ -8,22 +8,22 @@ const arreglo = [50, 60, 100];
 
 const multiplicar = (a, b, c) => a * b * c;
 
-console.log(multiplicar(...arreglo));
+//console.log(multiplicar(...arreglo));
 
 const numeros = [6, 7, 8];
-console.log(...numeros);
+//console.log(...numeros);
 
 const original = [100, 50, 65];
 const copia = [...original];
 
-console.log(copia);
+//console.log(copia);
 
 const array1 = [1, 2, 3];
 const array2 = [4, 5, 6];
 
 const concatenado = [...array1, ...array2];
 
-console.log(concatenado);
+//console.log(concatenado);
 
 //* Uso de spread en objetos
 const persona1a = { nombre: 'Juan', edad: 30 };
@@ -31,4 +31,46 @@ const persona1b = { ciudad: 'DF', profesion: 'Ingeniero de software' };
 const persona2 = { ...persona1a };
 const fusion = { ...persona1a, ...persona1b };
 
-console.log(fusion);
+//console.log(fusion);
+
+//  Gestion de almacenamiento en memoria
+/* 
+let a = 10;
+let b = a;
+
+console.log(a);
+console.log(b);
+console.log('****************************');
+
+a = 30;
+console.log(a);
+console.log(b);
+
+let aS = 'Juan';
+let bS = aS;
+
+console.log('****************************');
+console.log(aS);
+console.log(bS);
+
+aS = 'Rogelio';
+
+console.log('****************************');
+console.log(aS);
+console.log(bS); */
+
+let arreglo1 = [1, 2];
+let arreglo2 = arreglo1;
+//let arreglo2 = [...arreglo1]
+//let arreglo2 = arreglo1.slice()
+
+console.log('****************************');
+console.log(arreglo1);
+console.log(arreglo2);
+
+arreglo1.push(20);
+
+console.log('****************************');
+console.log(arreglo1);
+console.log(arreglo2);
+
